82ffe5f81f7f48c429d03f2bf76427d0df3d4624
LevinaMaria <6057721@gmail.com>
2024-05-23 12:50:07 +0200

    INITIAL COMMIT

---
+++ music_tables.sql
@@ -0,0 +1,43 @@
+CREATE TABLE IF NOT EXISTS genre (
+	genre_id SERIAL PRIMARY KEY,
+	genre_name VARCHAR(80)  NOT NULL	
+);
+
+CREATE TABLE IF NOT EXISTS singer (
+	singer_id SERIAL PRIMARY KEY,	
+	singer_name VARCHAR(40) NOT NULL	
+);
+
+CREATE TABLE IF NOT EXISTS genre_singer (	
+	genre_id INTEGER NOT NULL REFERENCES genre(genre_id),
+	singer_id INTEGER NOT NULL REFERENCES singer(singer_id)	
+);
+
+CREATE TABLE IF NOT EXISTS album (    
+	album_id SERIAL PRIMARY KEY,	
+	album_name VARCHAR(80) NOT NULL,
+    album_year INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS singer_album (	
+	album_id INTEGER NOT NULL REFERENCES album(album_id),
+	singer_id INTEGER NOT NULL REFERENCES singer(singer_id)	
+);
+
+CREATE TABLE IF NOT EXISTS track (    
+	track_id SERIAL PRIMARY KEY,	
+	track_name VARCHAR(80) NOT NULL,
+    track_duration INTEGER NOT NULL,
+    album_id INTEGER NOT NULL REFERENCES album(album_id),
+    CONSTRAINT fk_album UNIQUE (album_id)
+);
+
+CREATE TABLE IF NOT EXISTS collections (    
+	collection_id SERIAL PRIMARY KEY,	
+	collection_name VARCHAR(80) NOT NULL,
+    collection_year INTEGER NOT NULL
+);
+CREATE TABLE IF NOT EXISTS track_collection (	
+	track_id INTEGER NOT NULL REFERENCES track(track_id),
+	collection_id INTEGER NOT NULL REFERENCES collections(collection_id)	
+);
